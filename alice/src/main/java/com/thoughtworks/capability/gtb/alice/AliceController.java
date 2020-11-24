c51378ee4eab9c4194836a780d84dab21ac3cb5a
TadashiLee <584638605@qq.com>
2020-11-24 20:02:45 +0800

    [wenchang.li]feat:finish homework

---
+++ alice/src/main/java/com/thoughtworks/capability/gtb/alice/AliceController.java
@@ -0,0 +1,25 @@
+package com.thoughtworks.capability.gtb.alice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class AliceController {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @GetMapping("/hello")
+    public String getHello(){
+        String url = "http://localhost:8081/hello";
+        RestTemplate restTemplate = restTemplate();
+        ResponseEntity<String> responseEntity = restTemplate
+                .getForEntity(url,String.class);
+        return responseEntity.getBody();
+    }
+}
